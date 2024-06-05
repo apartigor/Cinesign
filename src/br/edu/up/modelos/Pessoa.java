c8495f563eab268bfd58883133d05da30c0f23b9
apartigor <contact.jojigor@gmail.com>
2024-06-05 10:03:10 -0300

    Class Abstrata Pessoa

--- src/br/edu/up/modelos/Pessoa.java
+++ src/br/edu/up/modelos/Pessoa.java
@@ -1,5 +1,16 @@
 package br.edu.up.modelos;
 
-public class Pessoa {
-    
+public abstract class Pessoa {
+    private String cpf;
+    private String nome;
+    private String email;
+    private String dataNasc;
+
+    public Pessoa(String cpf, String nome, String email, String dataNasc) {
+        this.cpf = cpf;
+        this.nome = nome;
+        this.email = email;
+        this.dataNasc = dataNasc;
+    }
+
 }
