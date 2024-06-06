fa1c280443cee739a463bc47f9297b6d24aeb9f1
Igor Hey Matos <iheymatos1@gmail.com>
2024-06-06 11:06:22 -0300

    Controle de filmes

--- src/br/edu/up/controles/ControleDeFilmes.java
+++ src/br/edu/up/controles/ControleDeFilmes.java
@@ -1,17 +1,29 @@
 package br.edu.up.controles;
 
 import br.edu.up.modelos.Filme;
+import br.edu.up.modelos.Pessoa;
 import br.edu.up.daos.GerenciadorDeAquivosDeFilmes;
 
 import java.util.List;
 
 public class ControleDeFilmes {
 
+    private List<Filme> filmes;
+    private GerenciadorDeAquivosDeFilmes gerenciadorDeArquivosFilmes;
 
-        private List<Filme> filmes;
-        private GerenciadorDeAquivosDeFilmes gerenciadorDeArquivosFilmes;
-    
-        public ControleDeFilmes(){
-                
-        }
+    public ControleDeFilmes() {
+
+    }
+
+    public List<Filme> listarFilmes() {
+        return filmes;
+    }
+
+    public void salvarFilmes() {
+        // gerenciadorDeArquivosFilmes.gravarArquivoCSV(filmes);
+    }
+
+    public void adicionarFilme(Filme filme) {
+        filmes.add(filme);
+    }
 }
